ff59cd298a3c27e6b2503c71b6815f0c8f60cf6f
Lythmass <lythmass17@gmail.com>
2022-12-27 15:33:46 +0400

    fix: add index.js in all folders

---
+++ src/components/Layout/index.js
@@ -0,0 +1,3 @@
+export { default as Layout } from './Layout';
+export { default as LayoutHeader } from './LayoutHeader';
+export { default as LayoutBody } from './LayoutBody';
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,2 @@
-export { default as InputText } from './Inputs/InputText';
-export { default as Layout } from './Layout/Layout';
-export { default as LayoutHeader } from './Layout/LayoutHeader';
-export { default as LayoutBody } from './Layout/LayoutBody';
+export { InputText } from './Inputs';
+export { Layout, LayoutHeader, LayoutBody } from './Layout';
