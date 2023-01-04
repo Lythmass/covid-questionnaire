f2601903a2eb83f45baaedc832695e95fc656249
Lythmass <lythmass17@gmail.com>
2023-01-04 18:01:10 +0400

    fix: background of inputs
    closes #20

--- src/components/Inputs/InputTextComponents/Date.jsx
+++ src/components/Inputs/InputTextComponents/Date.jsx
@@ -12,7 +12,7 @@ const Date = (props) => {
         type={isDate}
         id={props.name}
         placeholder={props.placeholder}
-        className='w-[31.25rem] h-[3.125rem] px-5 border-[0.8px] border-neutral-800 placeholder-neutral-800 bg-gray-200'
+        className='w-[31.25rem] h-[3.125rem] px-5 border-[0.8px] border-neutral-800 placeholder-neutral-800 bg-white-777'
       />
       <p className='text-orange-600'>
         {props.methods.formState.errors[props.name]?.message}
--- src/components/Inputs/InputTextComponents/Num.jsx
+++ src/components/Inputs/InputTextComponents/Num.jsx
@@ -7,7 +7,7 @@ const Num = (props) => {
         type={props.type}
         id={props.name}
         placeholder={props.placeholder}
-        className='w-[31.25rem] h-[3.125rem] px-5 border-[0.8px] border-neutral-800 placeholder-neutral-800 bg-gray-200'
+        className='w-[31.25rem] h-[3.125rem] px-5 border-[0.8px] border-neutral-800 placeholder-neutral-800 bg-white-777'
       />
       <p className='text-orange-600'>
         {props.methods.formState.errors[props.name]?.message}
